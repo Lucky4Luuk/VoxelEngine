b46254523cb1003184227967708b676e34188740
Luuk van Oijen <lazyluuk.channel@gmail.com>
2020-07-15 04:56:37 +0200

    bunch of explanation stuff for dags, and very very very basic start of an implementation
    this is so cool

--- voxel_dag/src/dag.rs
+++ voxel_dag/src/dag.rs
@@ -1,5 +1,34 @@
 use glam::*;
 
-pub struct Node {
-    
+//In the original paper, it suggests storing only a pointer to the first child, and then
+//have all the other children stored in memory consecutively, but I'm not sure how I'd do this
+//in Rust and I have to send it to the GPU anyway, which I plan on doing through an SSBO.
+//Therefore, I'm going to stick with an identical layout on the CPU, making use of indices
+//instead of pointers.
+
+//slice.as_ptr() as *const c_void
+//slice.len() as isize
+
+//Data structure:
+// childmask        [24 bits empty; 8 bits for mask]
+// child index 1    [usize]
+
+//In the above data structure, we only need the first index, as we can simply check the childmask
+//to see which children contain geometry, and so we only need to know where the first
+//non-empty child is located, as they are stored consecutively in the buffer.
+//This is essentially the same way the original paper did it, but we are essentially
+//using a buffer as memory here, which is mostlikely less efficient, but
+//it allows us to very easily upload this data to the GPU.
+
+//NOTE: It should not be super hard to eventually switch this out to use memory directly,
+//      but changing it now makes no sense. It's much easier to develop it like this as well,
+//      so I'll leave it like this for now :)
+pub struct DAG {
+    data: Vec<u32>, //raw data, only used internally
+}
+
+impl DAG {
+    pub fn from_voxel_data(vox_data: Vec<u8>) -> Self {
+        unimplemented!();
+    }
 }
